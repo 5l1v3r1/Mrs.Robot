8c7ad837b0648c283a7d80fb362165c1ef214f6d
^˵◕ω◕˵^ <low.lucyy@gmail.com>
2019-08-15 11:29:52 -0400

    Update model.js

--- fashion-mnist-vae/model.js
+++ fashion-mnist-vae/model.js
@@ -1,35 +1,12 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
-
-/**
- * This file implements the code for a multilayer perceptron based variational
- * autoencoder and is a per of this code
- * https://github.com/keras-team/keras/blob/master/examples/variational_autoencoder.py
- *
- * See this tutorial for a description of how autoencoders work.
- * https://blog.keras.io/building-autoencoders-in-keras.html
- */
+// Variational encoder implementation code
+// refers to: https://github.com/keras-team/keras/blob/master/examples/variational_autoencoder.py
 
 const tf = require('@tensorflow/tfjs');
 
 /**
  * The encoder portion of the model.
  *
- * @param {object} opts encoder configuration, includnig the following fields:
+ * @param {object} opts encoder configuration, including the following fields:
  *   - originaDim {number} Length of the input flattened image.
  *   - intermediateDim {number} Number of units of the intermediate (i.e.,
  *     hidden) dense layer.
@@ -39,6 +16,7 @@ const tf = require('@tensorflow/tfjs');
  * @param {number} opts.latentDim number of dimensions in latent space.
  * @returns {tf.LayersModel} the encoder model.
  */
+
 function encoder(opts) {
   const {originalDim, intermediateDim, latentDim} = opts;
 
