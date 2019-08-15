59c9bc19754e17e86bef6005df72556626eeec96
^˵◕ω◕˵^ <low.lucyy@gmail.com>
2019-08-15 11:14:21 -0400

    Update test_util.js

--- test_util.js
+++ test_util.js
@@ -1,5 +1,6 @@
-// Execute all unit tests in the current directory. Takes a jasmine_util from
-// tfjs-core so that we use the tfjs-core module from the right test directory.
+// Execute all unit tests in the current directory. 
+// Takes jasmine_util from tfjs-core.
+// Use the tfjs-core module from the right test directory.
 
 function runTests(jasmineUtil, specFiles) {
   // tslint:disable-next-line:no-require-imports
