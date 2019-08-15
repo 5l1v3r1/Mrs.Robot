0360176fc784cf6be2916ca07df23798c3baa476
^˵◕ω◕˵^ <low.lucyy@gmail.com>
2019-08-15 10:54:31 -0400

    Update data.js

--- fashion-mnist-vae/data.js
+++ fashion-mnist-vae/data.js
@@ -1,20 +1,3 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
-
 const tf = require('@tensorflow/tfjs');
 const assert = require('assert');
 const fs = require('fs');
@@ -40,7 +23,7 @@ const IMAGE_FLAT_SIZE = IMAGE_HEIGHT * IMAGE_WIDTH * IMAGE_CHANNELS;
  * @param {Buffer} buffer
  * @param {number} headerLength
  *
- * @returns {number[]} MNIST data header values
+ * @returns {number[]} fashion MNIST data header values
  */
 function loadHeaderValues(buffer, headerLength) {
   const headerValues = [];
@@ -54,7 +37,7 @@ function loadHeaderValues(buffer, headerLength) {
 /**
  * Load the images from the given file and normalize the data to 0-1 range.
  *
- * Input file should be in the MNIST/FashionMNSIT file format
+ * Input file should be in the fashion MNIST file format
  *
  * @param {string} filepath
  *
